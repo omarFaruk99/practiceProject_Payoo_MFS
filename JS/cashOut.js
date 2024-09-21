a9a89ca212879332ce858c02d16111107f5c28de
faruk <2035faruk@gmail.com>
2024-09-22 00:07:00 +0600

    update partial complete 'cashOut'

--- JS/cashOut.js
+++ JS/cashOut.js
@@ -1,12 +1,22 @@
-document.getElementById("btn-cashOut").addEventListener("click",function(){
+document.getElementById("btn-cashOut").addEventListener("click", function (event) {
+    event.preventDefault()
     let withdrawAmount = document.getElementById("inputField-cashOut").value
     withdrawAmount = parseFloat(withdrawAmount);
-    const inputFieldPinNumber = document.getElementById("inputField-pinNumber").value
+    const inputFieldPinNumber = document.getElementById("cashOut-inputField-pinNumber").value
 
     // pin number validation
-    if(inputFieldPinNumber === "999"){
+    if (inputFieldPinNumber === "999") {
+        let currentBalance = document.getElementById("current-balance").innerText
+        currentBalance = parseFloat(currentBalance);
+        let newBalance = currentBalance - withdrawAmount;
+        if(newBalance >=0){
 
-    }else{
+            document.getElementById("current-balance").innerText = newBalance;
+        }else{
+            alert("Insufficient Balance!")
+        }
+
+    } else {
         alert("Wrong Pin Number!")
     }
 })
